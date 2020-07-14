6055c6e813ae3649ee4fcfe66e43a3e53aa5881c
KileyAbbot <kileyabbot@gmail.com>
2020-07-14 11:39:06 -0400

    dsfadsda

---
+++ testgitty/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testgitty
+//
+//  Created by Kiley Abbot on 7/14/20.
+//  Copyright © 2020 Kiley Abbot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var changer: UILabel!
+    
+    @IBAction func thenut(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
